c492309e3c27448f79354d756768668456371fb7
hillary.dalie@gmail.com <hidalie@gmail.com>
2019-08-16 16:56:08 +0300

    Add tarehe component to Angular App

--- src/app/sema-kura.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SemaKuraDirective } from './sema-kura.directive';
-
-describe('SemaKuraDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SemaKuraDirective();
-    expect(directive).toBeTruthy();
-  });
-});
