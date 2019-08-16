2c4e59d1a335d4268fc0dbe902a88fc6af5393c4
hillary.dalie@gmail.com <hidalie@gmail.com>
2019-08-16 16:49:41 +0300

    Add sema-kura directive to the angular project

---
+++ src/app/sema-kura.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SemaKuraDirective } from './sema-kura.directive';
+
+describe('SemaKuraDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SemaKuraDirective();
+    expect(directive).toBeTruthy();
+  });
+});
