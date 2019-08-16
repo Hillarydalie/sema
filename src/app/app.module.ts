e16a7177e4cc9789138294a19c4c48ccf5a11c7d
hillary.dalie@gmail.com <hidalie@gmail.com>
2019-08-16 16:42:31 +0300

    Add maneno component to our app.module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SemaComponent } from './sema/sema.component';
 import { SemaFormComponent } from './sema-form/sema-form.component';
+import { SemaManenoComponent } from './sema-maneno/sema-maneno.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SemaComponent,
-    SemaFormComponent
+    SemaFormComponent,
+    SemaManenoComponent
   ],
   imports: [
     BrowserModule,
