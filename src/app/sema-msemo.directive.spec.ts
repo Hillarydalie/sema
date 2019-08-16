b2a95dd776056cd750264591a2f8f5d2ce1c7d34
hillary.dalie@gmail.com <hidalie@gmail.com>
2019-08-16 16:59:04 +0300

    Add directive files in the app folder

---
+++ src/app/sema-msemo.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SemaMsemoDirective } from './sema-msemo.directive';
+
+describe('SemaMsemoDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SemaMsemoDirective();
+    expect(directive).toBeTruthy();
+  });
+});
